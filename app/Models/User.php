820ab3e494a093adb332ac50d5ed2db581a129d5
ildyria <beviguier@gmail.com>
2023-04-23 15:18:00 +0200

    squash everything (clean up history)

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,6 @@ use Illuminate\Support\Facades\Auth;
 use Laragear\WebAuthn\Contracts\WebAuthnAuthenticatable;
 use Laragear\WebAuthn\Models\WebAuthnCredential;
 use Laragear\WebAuthn\WebAuthnAuthentication;
-use function Safe\mb_convert_encoding;
 
 /**
  * App\Models\User.
@@ -35,19 +34,12 @@ use function Safe\mb_convert_encoding;
  * @property bool                                                  $may_administrate
  * @property bool                                                  $may_upload
  * @property bool                                                  $may_edit_own_settings
- * @property string                                                $name
  * @property string|null                                           $token
  * @property string|null                                           $remember_token
  * @property Collection<BaseAlbumImpl>                             $albums
- * @property int|null                                              $albums_count
  * @property DatabaseNotificationCollection|DatabaseNotification[] $notifications
- * @property int|null                                              $notifications_count
  * @property Collection<BaseAlbumImpl>                             $shared
- * @property int|null                                              $shared_count
  * @property Collection<Photo>                                     $photos
- * @property int|null                                              $photos_count
- * @property Collection<int, WebAuthnCredential>                   $webAuthnCredentials
- * @property int|null                                              $web_authn_credentials_count
  */
 class User extends Authenticatable implements WebAuthnAuthenticatable
 {
@@ -81,6 +73,11 @@ class User extends Authenticatable implements WebAuthnAuthenticatable
 		'may_upload' => 'boolean',
 		'may_edit_own_settings' => 'boolean',
 	];
+
+	protected function _toArray(): array
+	{
+		return parent::toArray();
+	}
 
 	/**
 	 * Return the albums owned by the user.
@@ -124,8 +121,7 @@ class User extends Authenticatable implements WebAuthnAuthenticatable
 	 */
 	public function username(): string
 	{
-		// @phpstan-ignore-next-line This is temporary and should hopefully be fixed soon by Safe with proper type hinting.
-		return mb_convert_encoding($this->username, 'UTF-8');
+		return utf8_encode($this->username);
 	}
 
 	/**
